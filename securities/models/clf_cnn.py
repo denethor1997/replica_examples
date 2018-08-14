8a272651a4271302a86a8a3741b44328f3815085
Jian Liang <denethor2014@163.com>
2018-08-14 19:10:43 +0800

    bn

--- securities/models/clf_cnn.py
+++ securities/models/clf_cnn.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
 from keras.optimizers import Adam
 from keras.layers.advanced_activations import PReLU
 
@@ -72,6 +73,38 @@ def clf_cnn_prelu_categorical(input_shape, lr=0.0001, out_dim=2):
 
     model.add(Flatten())
     model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(out_dim, activation='softmax'))
+
+    adam = Adam(lr=lr)
+    model.compile(optimizer=adam,
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
+
+def clf_cnn_prelu_categorical_bn(input_shape, lr=0.0001, out_dim=2):
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape, padding='same'))
+    model.add(BatchNormalization())
+    model.add(PReLU())
+    model.add(Conv2D(32, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(PReLU())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(PReLU())
+    model.add(Conv2D(64, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(PReLU())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(BatchNormalization())
     model.add(Dropout(0.5))
     model.add(Dense(out_dim, activation='softmax'))
 
