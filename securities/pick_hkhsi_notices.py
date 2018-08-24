296dbc1d139859c79d74ae3800f6727c4d778c38
Jian Liang <denethor2014@163.com>
2018-08-24 15:18:38 +0800

    pick hkhsi notices script

---
+++ securities/pick_hkhsi_notices.py
@@ -0,0 +1,55 @@
+import time
+import os
+import numpy as np
+import pandas as pd
+import tushare as ts
+from utils.netease import *
+from datetime import datetime
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--hkhsi', type=str, required=True)
+args = parser.parse_args()
+
+
+today = datetime.now().strftime('%Y-%m-%d')
+
+
+timestr = datetime.now().strftime('%Y-%m-%d')
+log_path = os.path.join('snapshots_pick', 'pick_notices_%s.log'%timestr)
+log = open(log_path, 'w')
+
+def get_notices_by_code(code):
+    try:
+        notices = ts.get_notices(code=str(code), date=today)
+        return notices
+    except Exception as e:
+        print(e)
+        return None
+
+for line in open(args.hkhsi):
+    tokens = line.strip().split('@')
+    code = tokens[0]
+    score = tokens[1]
+    date = tokens[2]
+
+    if str(code).startswith('3'):
+        continue
+
+    time.sleep(0.2)
+    print('getting notices for %s'%code)
+
+    notes = get_notices_by_code(code)
+    if notes is None or notes.empty:
+        continue
+
+    titles = ''
+    for index, row in notes.iterrows():
+        titles += row['title'].encode('utf-8') + ';'
+
+    log.write('%s,%s,%s,%s\n'%(code, score, date, titles))
+
+#update_netease_by_code(600082)
+
+log.close()
